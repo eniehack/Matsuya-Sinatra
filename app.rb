126eddff046d32fb8f88e5db0fb20cba501967b1
Eniehack <studio4units@gmail.com>
2018-04-28 00:13:33 +0900

    ドリンク一覧をベースに牛めし、朝食、サイドメニュー、定食、うどん、トッピング、プレミアム、それぞれの一覧ページでの挙動も追記

--- app.rb
+++ app.rb
@@ -31,3 +31,59 @@ get '/drink' do
   @result = JSON.parse(json)
   erb :list
 end
+
+get '/gyuumeshi' do
+  @title = '牛めし一覧'
+  uri = URI.parse('https://matsuya.makotia.me/v4/search?type=gyuumeshi')
+  json = Net::HTTP.get(uri)
+  @result = JSON.parse(json)
+  erb :list
+end
+
+get '/morning' do
+  @title = '朝食一覧'
+  uri = URI.parse('https://matsuya.makotia.me/v4/search?type=moaning')
+  json = Net::HTTP.get(uri)
+  @result = JSON.parse(json)
+  erb :list
+end
+
+get '/side' do
+  @title = 'サイドメニュー一覧'
+  uri = URI.parse('https://matsuya.makotia.me/v4/search?type=sidemenu')
+  json = Net::HTTP.get(uri)
+  @result = JSON.parse(json)
+  erb :list
+end
+
+get '/teishoku' do
+  @title = '定食一覧'
+  uri = URI.parse('https://matsuya.makotia.me/v4/search?type=teishuoku')
+  json = Net::HTTP.get(uri)
+  @result = JSON.parse(json)
+  erb :list
+end
+
+get '/udon' do
+  @title = 'うどん一覧'
+  uri = URI.parse('https://matsuya.makotia.me/v4/search?type=udon')
+  json = Net::HTTP.get(uri)
+  @result = JSON.parse(json)
+  erb :list
+end
+
+get '/topping' do
+  @title = 'トッピング一覧'
+  uri = URI.parse('https://matsuya.makotia.me/v4/search?type=topping')
+  json = Net::HTTP.get(uri)
+  @result = JSON.parse(json)
+  erb :list
+end
+
+get '/premium' do
+  @title = 'プレミアム一覧'
+  uri = URI.parse('https://matsuya.makotia.me/v4/search?type=premium')
+  json = Net::HTTP.get(uri)
+  @result = JSON.parse(json)
+  erb :list
+end
